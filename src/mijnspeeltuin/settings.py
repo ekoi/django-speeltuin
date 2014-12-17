ac960be739bd89dee5a55cc74cbe737bfbfa74ca
ekoi <eko.indarto@dans.knaw.nl>
2014-12-17 08:27:08 +0100

    add signup form

--- src/mijnspeeltuin/settings.py
+++ src/mijnspeeltuin/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+#BASE_DIR = /Users/akmi/git/django-speeltuin/src
 
 
 # Quick-start development settings - unsuitable for production
@@ -36,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'signups',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -80,3 +82,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Template location
+TEMPLATE_DIRS = (
+    os.path.join(os.path.dirname(BASE_DIR), "static", "templates"),
+    #'/Users/akmi/git/django-speeltuin/src',
+)
+
--- src/mijnspeeltuin/urls.py
+++ src/mijnspeeltuin/urls.py
@@ -5,7 +5,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'mijnspeeltuin.views.home', name='home'),
+    url(r'^$', 'signups.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
